74782ff565e17d7df8ccda11a2a1fd3b43c91d2d
angel <angelluisvn@gmail.com>
2022-04-25 14:10:46 +0200

    Actualizacion

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.getElementById("invisible").style.display = "none";
+document.getElementById("invisible").style.visibility = "hidden";
+document.getElementById("invisible2").style.visibility = "hidden";
+document.getElementById("invisible3").style.visibility = "hidden";
